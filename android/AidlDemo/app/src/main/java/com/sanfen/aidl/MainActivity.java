585c5d942089f120e6ea1e5fef2b0bab293da1a6
mazhou <mazhou@tuya.com>
2019-08-06 19:27:20 +0800

    aidodemo

--- android/AidlDemo/app/src/main/java/com/sanfen/aidl/MainActivity.java
+++ android/AidlDemo/app/src/main/java/com/sanfen/aidl/MainActivity.java
@@ -1,32 +1,61 @@
 package com.sanfen.aidl;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.time.temporal.ValueRange;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    IMyAidlInterface iMyAidlInterface;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        MyIntentService service = new MyIntentService();
-
         Intent intent = new Intent(this, MyIntentService.class);
-        bindService(intent, new ServiceConnection() {
+        bindService(intent, conn, Context.BIND_AUTO_CREATE);
+        findViewById(R.id.btn_hello).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-
+            public void onClick(View v) {
+                if (iMyAidlInterface != null) {
+                    try {
+                        Log.d(TAG, String.valueOf(iMyAidlInterface.add(1, 1)));
+                        Log.d(TAG, String.valueOf(iMyAidlInterface.add(2, 3)));
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
+        });
+    }
 
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
+    private ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            iMyAidlInterface = IMyAidlInterface.Stub.asInterface(service);
+        }
 
-            }
-        }, 0);
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            iMyAidlInterface = null;
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(conn);
     }
 }
--- android/AidlDemo/app/src/main/java/com/sanfen/aidl/MyIntentService.java
+++ android/AidlDemo/app/src/main/java/com/sanfen/aidl/MyIntentService.java
@@ -2,26 +2,10 @@ package com.sanfen.aidl;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.content.Context;
-import android.content.ServiceConnection;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.support.annotation.Nullable;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
- */
 public class MyIntentService extends IntentService {
-
-    private static final String ACTION_FOO = "com.sanfen.aidl.action.FOO";
-    private static final String ACTION_BAZ = "com.sanfen.aidl.action.BAZ";
-
-    private static final String EXTRA_PARAM1 = "com.sanfen.aidl.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "com.sanfen.aidl.extra.PARAM2";
 
     public MyIntentService() {
         super("MyIntentService");
@@ -29,11 +13,15 @@ public class MyIntentService extends IntentService {
 
     IMyAidlInterface.Stub stub = new IMyAidlInterface.Stub() {
         @Override
-        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
+        public int add(int aInt, int bInt) {
+            return aInt + bInt;
+        }
 
+        @Override
+        public int min(int aInt, int bInt) {
+            return aInt < bInt ? aInt : bInt;
         }
     };
-
 
     @Nullable
     @Override
@@ -51,70 +39,8 @@ public class MyIntentService extends IntentService {
         super.onDestroy();
     }
 
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, MyIntentService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, MyIntentService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
-            }
-        }
     }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        try {
-            stub.basicTypes(0,0, true, 0, 0, "");
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 }
